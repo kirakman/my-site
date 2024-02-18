b4111c08f578c06ed8f839171223be4e98f1bc36
kirakman <dourado178@gmail.com>
2024-02-18 13:54:08 -0300

    rename imgs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { Container1Component } from './components/container1/container1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    Container1Component
   ],
   imports: [
     BrowserModule,
