454c8825f9d5687a263c4d438b3d1386a7643e9d
kirakman <dourado178@gmail.com>
2024-03-03 17:31:05 -0300

    finish all containers content

--- src/app/components/container4/container4.component.ts
+++ src/app/components/container4/container4.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-container4',
@@ -7,5 +6,9 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./container4.component.css']
 })
 export class Container4Component {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = [
+    { src: '/assets/img/Projetos/Marketing Uniceplac.JPG', alt: 'Site Marketing', captionTitle: 'Site Marketing UNICEPLAC', captionText: 'Front end feito em Angular 16 e Backend feito com Java Spring Boot, esse projeto foi disponibilizado para registro de eventos da própria faculdade que tinha como objetivo informar os estudantes e público no geral. ' },
+    { src: '/assets/img/Projetos/Mundo invertido Stranger Things.JPG', alt: 'Site Stranger Things', captionTitle: 'Mundo Invetido Stranger Things', captionText: 'Projeto feito com HTML, CSS e JS puro com duas templates, dark mode e light mode, em homenagem à série Stranger Things, com formulário reativo enviando dados para backend Firebase.' },
+    { src: '/assets/img/Projetos/Pokedex.png', alt: 'Pokédex', captionTitle: 'Clássica Pokédex', captionText: 'Pokédex feita em Angular com requisição em API, utilizando alguns conceitos de Infinity Scroll, Lazy Loading e Filter Pipe.' }
+  ];
 }
