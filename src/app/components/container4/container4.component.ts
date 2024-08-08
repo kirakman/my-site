11caf839cd41e47b6dba35a9401bf8a4accc9a9c
kirakman <dourado178@gmail.com>
2024-08-08 16:41:07 -0300

    new update

--- src/app/components/container4/container4.component.ts
+++ src/app/components/container4/container4.component.ts
@@ -13,5 +13,6 @@ export class Container4Component {
     { src: '/assets/img/Projetos/Marketing Uniceplac.JPG', alt: 'Site Marketing', captionTitle: 'Site Marketing UNICEPLAC', captionText: 'Front end feito em Angular 16 e Backend feito com Java Spring Boot, esse projeto foi disponibilizado para registro de eventos da própria faculdade que tinha como objetivo informar os estudantes e público no geral. ', url: 'https://github.com/kirakman/ProjetoMarketing'},
     { src: '/assets/img/Projetos/Mundo invertido Stranger Things.JPG', alt: 'Site Stranger Things', captionTitle: 'Mundo Invetido Stranger Things', captionText: 'Projeto feito com HTML, CSS e JS puro com duas templates, dark mode e light mode, em homenagem à série Stranger Things, com formulário reativo enviando dados para backend Firebase.', url: 'https://github.com/kirakman/Landing_Page_Stranger_Things_DIO' },
     { src: '/assets/img/Projetos/Pokedex.png', alt: 'Pokédex', captionTitle: 'Clássica Pokédex', captionText: 'Pokédex feita em Angular com requisição em API, utilizando alguns conceitos de Infinity Scroll, Lazy Loading e Filter Pipe.', url: 'https://github.com/kirakman/pokedex' },
+    { src: '/assets/img/Projetos/UniteSummit.jpg', alt: 'Unite Summit', captionTitle: 'Unite Summit', captionText: 'Unite Summit em React Native + EXPO GO', url: 'https://github.com/kirakman/nwl-unite' },
   ];
 }
